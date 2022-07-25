42a30ca9d66a72931fbad78803811c219653adfb
WIN 10 Desktop GVR <gsenandika@gmail.com>
2022-07-25 12:40:05 +0700

    add select item on role api

--- app/Controllers/LIT/RoleController.php
+++ app/Controllers/LIT/RoleController.php
@@ -28,7 +28,7 @@ class RoleController extends BaseController
     {
         $adminRoles = new AdminRole();
 
-        $get = $adminRoles->select('rol_id, rol_nama, rol_status, adm_create.adm_nama as kreator, adm_update.adm_nama as editor')
+        $get = $adminRoles->select('rol_id, rol_nama, rol_status, rol_list_modul, rol_list_menu, adm_create.adm_nama as creator, adm_update.adm_nama as updater')
                           ->join('admin as adm_create', 'rol_created_by = adm_create.adm_id', 'left')
                           ->join('admin as adm_update', 'rol_updated_by = adm_update.adm_id', 'left')
                           ->where('rol_id', $id)
@@ -42,6 +42,23 @@ class RoleController extends BaseController
                 'title'   => 'Gagal Mengambil Data',
                 'message' => 'Data modul tidak ditemukan'
             ]);
+        }
+
+        // parse data
+        if (!is_array(json_decode($get['rol_list_modul'], TRUE)))
+        {
+            $modul = new AdminModul();
+            $modul = $modul->select('mod_id')->findAll();
+
+            $get['rol_list_modul'] = json_encode(array_column($modul, 'mod_id'));
+        }
+
+        if (!is_array(json_decode($get['rol_list_menu'], TRUE)))
+        {
+            $menu = new AdminMenu();
+            $menu = $menu->select('men_id')->findAll();
+
+            $get['rol_list_menu'] = json_encode(array_column($menu, 'men_id'));
         }
 
         // return
