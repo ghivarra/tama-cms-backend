e352f423dc91ab133950c0df25c93dd3cc7aa65d
WIN 10 Desktop GVR <gsenandika@gmail.com>
2022-07-26 11:16:23 +0700

    add update admin api

--- app/Controllers/LIT/AdminController.php
+++ app/Controllers/LIT/AdminController.php
@@ -26,11 +26,9 @@ class AdminController extends BaseController
     {
         $admin = new Admin();
 
-        $get = $admin->select('adm_id, adm_nama, adm_status, adm_create.adm_nama as kreator, adm_update.adm_nama as editor')
-                          ->join('admin as adm_create', 'adm_created_by = adm_create.adm_id', 'left')
-                          ->join('admin as adm_update', 'adm_updated_by = adm_update.adm_id', 'left')
-                          ->where('adm_id', $id)
-                          ->first();
+        $get = $admin->select('adm_id, adm_nama, adm_email, adm_status, adm_role')
+                     ->where('adm_id', $id)
+                     ->first();
 
         if (empty($get))
         {
@@ -335,33 +333,101 @@ class AdminController extends BaseController
     public function update()
     {
         $data = [
-            'adm_id'     => $this->request->getPost('adm_id'),
-            'adm_nama'   => $this->request->getPost('adm_nama'),
-            'adm_status' => $this->request->getPost('adm_status')
+            'adm_id'       => $this->request->getPost('adm_id'),
+            'adm_nama'     => $this->request->getPost('adm_nama'),
+            'adm_status'   => $this->request->getPost('adm_status'),
+            'adm_email'    => $this->request->getPost('adm_email'),
+            'adm_password' => $this->request->getPost('adm_password'),
+            'adm_role'     => $this->request->getPost('adm_role'),
         ];
 
-        // validasi
-        $validation = Services::validation();
-        $validation->setRules([
-            'adm_id'     => ['label' => 'Modul', 'rules' => 'required|numeric|is_not_unique[admin_modul.adm_id]'],
-            'adm_nama'   => ['label' => 'Nama Admin', 'rules' => 'required|max_length[120]'],
-            'adm_status' => ['label' => 'Status', 'rules' => 'required|in_list[aktif,nonaktif]'],
-        ]);
+        // get admin
+        $admin = new Admin();
+        $old   = $admin->where('adm_id', $data['adm_id'])->first();
 
-        // run
-        if (!$validation->run($data))
+        if (empty($old))
         {
             return $this->response->setStatusCode(400)->setJSON([
                 'code'    => 400,
                 'status'  => 'error',
                 'title'   => 'Gagal Menyimpan Data',
-                'message' => implode(', ', $validation->getErrors())
+                'message' => 'Admin tidak ditemukan'
             ]);
+        }
+
+        // set rules
+        $rules = [
+            'adm_id'     => ['label' => 'Admin', 'rules' => 'required|numeric'],
+            'adm_nama'   => ['label' => 'Nama Admin', 'rules' => 'required|max_length[120]'],
+            'adm_status' => ['label' => 'Status', 'rules' => 'required|in_list[aktif,nonaktif]'],
+            'adm_role'     => ['label' => 'Role', 'rules' => 'required|numeric|is_not_unique[admin_role.rol_id]'],
+        ];
+
+        if ($data['adm_email'] != $old['adm_email'])
+        {
+            $rules['adm_email'] = ['label' => 'Email', 'rules' => 'required|valid_email|is_unique[admin.adm_email]'];
+
+        } else {
+
+            $rules['adm_email'] = ['label' => 'Email', 'rules' => 'required|valid_email'];
+        }
+
+        // change password
+        if (!empty($data['adm_password']))
+        {
+            $rules['adm_password'] = ['label' => 'Password', 'rules' => 'required|min_length[10]'];
+        }
+
+        // validasi
+        $validation = Services::validation();
+        $validation->setRules($rules);
+
+        // run
+        if (!$validation->run($data))
+        {
+            $errors = $validation->getErrors();
+
+            if (isset($rules['adm_password']))
+            {
+                if (!preg_match("#[0-9]+#", $data['adm_password']))
+                {
+                    $errors['adm_password'] = 'Password harus menggunakan huruf dan angka';
+                }
+            }
+
+            return $this->response->setStatusCode(400)->setJSON([
+                'code'    => 400,
+                'status'  => 'error',
+                'title'   => 'Gagal Menyimpan Data',
+                'message' => implode(', ', $errors)
+            ]);
+
+        } elseif (isset($rules['adm_password'])) {
+
+            if (!preg_match("#[0-9]+#", $data['adm_password']))
+            { 
+                return $this->response->setStatusCode(400)->setJSON([
+                    'code'    => 400,
+                    'status'  => 'error',
+                    'title'   => 'Gagal Menyimpan Data',
+                    'message' => 'Password harus menggunakan huruf dan angka'
+                ]);
+            }
         }
 
         // now unset unused data
         $id = $data['adm_id'];
         unset($data['adm_id']);
+
+        // parse password
+        if (isset($rules['adm_password']))
+        {
+            $data['adm_password'] = password_hash($data['adm_password'], PASSWORD_DEFAULT);
+
+        } else {
+
+            unset($data['adm_password']);
+        }
 
         $admin = new Admin();
         $admin->update($id, $data);
